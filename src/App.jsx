20c4ad7d411bdd8919fe6eba736717c0d58aafe7
Kirsti Esch <miss.k.esch@gmail.com>
2024-09-13 13:54:53 -0400

    setup bookbuddy

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { Routes } from "react-router-dom";
+
+function App() {
+  const [token, setToken] = useState(null);
+  useEffect(() => {
+    const localToken = localStorage.getItem("token");
+    if (localToken) {
+      setToken(localToken);
+    }
+  }, []);
+
+  return (
+    <>
+      <h1>Book Buddy</h1>
+      <Routes></Routes>
+    </>
+  );
+}
+
+export default App;
+
+{
+  /* <p>Complete the React components needed to allow users to browse a library catalog, check out books, 
+  review their account, and return books that they've finished reading.</p>
+
+<p>You may need to use the `token` in this top-level component in other components that need to know 
+if a user has logged in or not.</p>
+
+<p>Don't forget to set up React Router to navigate between the different views of your single page application!</p> */
+}
